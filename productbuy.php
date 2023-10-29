b32a7d5d688649dd9bf2834f61b7bd99e6806e7b
KurakiriYami <102347459+jirayuschompen@users.noreply.github.com>
2023-10-29 16:23:00 +0700

    add botton to product

---
+++ productbuy.php
@@ -0,0 +1,267 @@
+<?php
+    session_start();
+
+    require 'conn.php';
+    $sql = "SELECT * FROM product";
+    $result = $conn->query($sql);
+    if(!$result){
+        die("Error : ". $conn->$conn_error);
+    }
+
+    if (isset($_GET['logout'])) {
+      session_destroy();
+      unset($_SESSION['username']);
+      header('location: index.php');
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nippon2u</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"> 
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="css/headers.css" > -->
+    <link rel="stylesheet" href="css/sidebars.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/navbar.css"> -->
+    
+    <style>
+      
+      .btn.btn-primary.mb-2 {
+          margin-left: 10px;  /* Adjust the value as needed */
+          -right: 10px; /* Adjust the value as needed */
+}
+      .product-card {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+      }
+
+      /* Set a fixed height for the product image */
+      .product-image {
+          height: 510px; /* Adjust the height as per your requirements */
+          object-fit: cover; /* Maintain aspect ratio and cover the entire container */
+      }
+
+      /*  */
+      @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600&family=Sarabun:ital,wght@0,200;0,300;0,400;0,600;1,200;1,300;1,400;1,500;1,600&display=swap');
+
+      *{
+          font-family: 'Sarabun', sans-serif;
+      }
+      
+      .bg-mix-dark-1{
+          background-color: #f1f1f1 !important;
+          color: #000000 !important;
+      }
+      
+      .table>:not(caption)>*>*{
+          background-color: initial !important; /* หมายถึง ไม่มีการกำหนดค่า */
+          color: #fff !important;
+      }
+      
+      ///////////////
+
+
+    </style>
+</head>
+<body>
+    
+    <!-- header  -->
+
+      <nav class="navbar">
+      <div class="logo_item">
+        <i class="bx bx-menu" id="sidebarOpen"></i>
+        <!-- <img src="images/logo.png" alt=""> -->
+      </i>Nippon2u
+      </div>
+      <!-- <div class="search_bar">
+                <input type="text" id="product-search" placeholder="Search by Product Name" />
+            </div> -->
+
+      <div class="navbar_content">
+        <i class="bi bi-grid"></i>
+        <i class='bx bx-sun' id="darkLight"></i>
+        <i class='bx bx-bell' ></i>
+        <!-- <img src="images/profile.jpg" alt="" class="profile" /> -->
+        <?php if (isset($_SESSION['success'])) : ?>
+            <div class="success">
+                <h3>
+                    <?php 
+                        // echo $_SESSION['success'];
+                        // unset($_SESSION['success']);
+                    ?>
+                </h3>
+            </div>
+        <?php endif ?>
+    
+        <!-- logged in user information -->
+        <?php if (isset($_SESSION['username'])) : ?>
+            
+                <i>
+                    <?php 
+                        echo $_SESSION['username']; 
+                    ?>
+                </i>
+            
+
+                <i><a href="index.php?logout=1" class='bx bx-log-out' style="text-decoration: none; color : #707070">
+                  
+                </a></i>
+
+
+
+            <?php else : ?>
+                <!-- Display the "Login" and "Register" buttons -->
+                <i><a href="login.php">Login</a></i>
+                <i><a href="register.php">Register</a></i>
+        <?php endif ?>
+        </div>
+    </nav>
+    <!-- sidebar -->
+    <nav class="sidebar">
+      <div class="menu_content">
+        <ul class="menu_items">
+          <br>
+          <!-- <div class="menu_title menu_home"></div> -->
+          <!-- duplicate or remove this li tag if you want to add or remove navlink with submenu -->
+          <!-- start -->
+          <li class="item">
+            <div href="#" class="nav_link submenu_item">
+              <span class="navlink_icon">
+                <i class="bx bx-home-alt"></i>
+              </span>
+              <span class="navlink">Home</span>
+              <i class="bx bx-chevron-right arrow-left"></i>
+            </div>
+            <ul class="menu_items submenu">
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+            </ul>
+          </li>
+          <!-- end -->
+          <!-- duplicate this li tag if you want to add or remove  navlink with submenu -->
+          <!-- start -->
+          <li class="item">
+            <div href="#" class="nav_link submenu_item">
+              <span class="navlink_icon">
+                <i class="bx bx-grid-alt"></i>
+              </span>
+              <span class="navlink">Overview</span>
+              <i class="bx bx-chevron-right arrow-left"></i>
+            </div>
+            <ul class="menu_items submenu">
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+              <a href="#" class="nav_link sublink">Nav Sub Link</a>
+            </ul>
+          </li>
+          <!-- end -->
+        </ul>
+        <ul class="menu_items">
+          <!-- <div class="menu_title menu_editor"></div> -->
+          <!-- duplicate these li tag if you want to add or remove navlink only -->
+          <!-- Start -->
+          <!-- <li class="item">
+            <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                <i class="bx bxs-magic-wand"></i>
+              </span>
+              <span class="navlink">Magic build</span>
+            </a>
+          </li> -->
+          <!-- End -->
+          <!-- <li class="item">
+            <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                <i class="bx bx-loader-circle"></i>
+              </span>
+              <span class="navlink">Filters</span>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                <i class="bx bx-filter"></i>
+              </span>
+              <span class="navlink">Filter</span>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                <i class="bx bx-cloud-upload"></i>
+              </span>
+              <span class="navlink">Upload new</span>
+            </a>
+          </li>
+        </ul> -->
+        <ul class="menu_items">
+          <div class="menu_title menu_setting"></div>
+          <li class="item">
+            <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                <i class="bx bx-flag"></i>
+              </span>
+              <span class="navlink">Notice board</span>
+            </a>
+          </li>
+          <li class="item">
+            <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                <i class="bx bx-cog"></i>
+              </span>
+              <span class="navlink">Setting</span>
+            </a>
+          </li>
+          
+        </ul>
+        <!-- Sidebar Open / Close -->
+        <div class="bottom_content">
+          <div class="bottom expand_sidebar">
+            <!-- <span> Expand</span> -->
+            <i class='bx bx-log-in' ></i>
+          </div>
+          <div class="bottom collapse_sidebar">
+            <!-- <span> Collapse</span> -->
+            <i class='bx bx-log-out'></i>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+  <!-- End Side bar -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/search.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sidebars.js"></script>
+</body>
+</html>
+
+
+
+
+
+
